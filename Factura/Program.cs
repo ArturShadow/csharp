28ab9410db7f2147d33bae0fa418cfa7ffd61cb4
ArturShadow <41841053+ArturShadow@users.noreply.github.com>
2020-11-23 16:58:31 -0700

    Factura

---
+++ Factura/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Factura
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int noArticulos = 0, i;
+            double subtot = 0;
+            Console.WriteLine("Cuantos articulos se van a capturar");
+            noArticulos = int.Parse(Console.ReadLine());
+            int[] cantidad = new int[noArticulos];
+            string[] descripcion = new string[noArticulos];
+            double[] subtotal = new double[noArticulos];
+            int[] precio = new int[noArticulos];
+            
+            for (i = 0; i < noArticulos; i++)
+            {
+                Console.Write("Descripcion: ");
+                descripcion[i] = Console.ReadLine();
+
+                Console.Write("Cantidad: ");
+                cantidad[i] = int.Parse(Console.ReadLine());
+                Console.Write("Precio: ");
+                precio[i] = int.Parse(Console.ReadLine());
+                
+                subtotal[i] = cantidad[i] * precio[i];
+                subtot = subtot + subtotal[i];
+            }
+                Console.WriteLine("Cantidad       Descripcion   Precio      Subtotal");
+            for(i = 0; i < noArticulos; i++){
+                Console.WriteLine(" {0:D3}                {1}       {2}       {3} ", cantidad[i], descripcion[i], precio[i], subtotal[i]);
+            }  
+            Console.WriteLine("Subtotal: " + subtot);
+            Console.WriteLine("IVA: " + (subtot*.16));
+            Console.WriteLine("Total: " + (subtot+(subtot*.16)));
+            
+                  //cantidad descricpion precio subtotal
+                    //   1     chiccle    5      5
+ 
+                         //subtotal 104
+                         //iva       17
+                         //total     121
+            //presentar subtotal iva16 y total
+        }
+    }
+}
